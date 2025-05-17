51ead336d448d40621ed0dc80cb974783c5f642b
Đinh Huỳnh Gia Bảo <giabaoctg@gmail.com>
2025-05-17 15:38:00 +0700

    update(lab2): update features for security group

--- aws-infr/modules/security/main.tf
+++ aws-infr/modules/security/main.tf
@@ -1,4 +1,6 @@
 #Create security groups for public EC2 instances
+// checkov:skip=CKV_AWS_24: Need to open SSH for lab/demo
+// checkov:skip=CKV2_AWS_5: This SG is already attached to EC2 via module
 resource "aws_security_group" "public_ec2_sg" {
     name        = var.public_ec2_sg_name
     vpc_id      = var.vpc_id
@@ -9,17 +11,28 @@ resource "aws_security_group" "public_ec2_sg" {
         to_port     = 22
         protocol    = "tcp"
         cidr_blocks = [var.allowed_ssh_cidr]
+        description = "Allow SSH from trusted IP"
     }
 
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow HTTP outbound"
     }
+
+    egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS outbound"
+  }
 }
 
 # Create security groups for private EC2 instances
+// checkov:skip=CKV2_AWS_5: This SG is already attached to EC2 via module
 resource "aws_security_group" "private_ec2_sg" {
     name        = var.private_ec2_sg_name
     vpc_id      = var.vpc_id
@@ -30,6 +43,7 @@ resource "aws_security_group" "private_ec2_sg" {
         to_port     = 22
         protocol    = "tcp"
         cidr_blocks = [var.vpc_cidr_block]
+        description = "Allow SSH from VPC"
     }
     
     ingress {
@@ -37,6 +51,7 @@ resource "aws_security_group" "private_ec2_sg" {
         to_port     = 80
         protocol    = "tcp"
         cidr_blocks = [var.vpc_cidr_block]
+        description = "Allow HTTP from VPC"
     }
 
     ingress {
@@ -44,17 +59,20 @@ resource "aws_security_group" "private_ec2_sg" {
         to_port    = -1
         protocol   = "icmp"
         cidr_blocks = [var.vpc_cidr_block]
+        description = "Allow ICMP from VPC"
     }
 
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS outbound"
     }
 }
 
 # Create a default security group for the VPC
+// checkov:skip=CKV2_AWS_5: Default SG does not need to be tied to a specific resource
 resource "aws_security_group" "default_sg" {
     name        = var.default_sg_name
     vpc_id      = var.vpc_id
@@ -65,12 +83,14 @@ resource "aws_security_group" "default_sg" {
         to_port     = 0
         protocol    = "-1"
         cidr_blocks = [var.vpc_cidr_block]
+        description = "Allow all inbound traffic from VPC"
     }
 
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS outbound"
     }
 }
