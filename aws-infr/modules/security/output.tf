479e65601d20396af4d973c91f9fcb167606f6d3
Đinh Huỳnh Gia Bảo <giabaoctg@gmail.com>
2025-05-25 23:15:09 +0700

    update(lab2): delete default sg

--- aws-infr/modules/security/output.tf
+++ aws-infr/modules/security/output.tf
@@ -12,8 +12,3 @@ output "private_ec2_sg_name" {
     description = "The name of the private EC2 security group"
     value       = aws_security_group.private_ec2_sg.name
 }
-
-output "default_sg_name" {
-    description = "The name of the default security group"
-    value       = aws_security_group.default_sg.name
-}
