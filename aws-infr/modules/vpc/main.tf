9219f6586377f0338b9c96862be7076d6084333c
Đinh Huỳnh Gia Bảo <giabaoctg@gmail.com>
2025-06-04 20:04:36 +0700

    remove(lab2-vpc): remove redundant SG

--- aws-infr/modules/vpc/main.tf
+++ aws-infr/modules/vpc/main.tf
@@ -93,36 +93,6 @@ resource "aws_route_table_association" "private" {
     route_table_id = aws_route_table.private.id
 }
 
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port         = 0
-    to_port           = 0
-    protocol          = "-1"
-    cidr_blocks       = []
-    ipv6_cidr_blocks  = []
-    security_groups   = []
-    prefix_list_ids   = []
-    description       = "Deny all inbound"
-  }
-
-  egress {
-    from_port         = 0
-    to_port           = 0
-    protocol          = "-1"
-    cidr_blocks       = []
-    ipv6_cidr_blocks  = []
-    security_groups   = []
-    prefix_list_ids   = []
-    description       = "Deny all outbound"
-  }
-
-  tags = {
-    Name = "Default SG - deny all"
-  }
-}
-
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
     name              = "/aws/vpc/flow-logs/${aws_vpc.main.id}"
     retention_in_days = 365
